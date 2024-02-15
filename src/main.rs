124a3e37f0cd20b91531a40e9f3d77310c111350
Kannan Palani <kannanpalani455@gmail.com>
2024-02-15 22:04:50 +0530

    add error handling and file structure code

--- src/main.rs
+++ src/main.rs
@@ -1,26 +1,23 @@
-
+use mini_grep::run;
+use mini_grep::Config;
 use std::env;
-use std::fs;
+use std::process;
 
 fn main() {
     println!("Hello, world!");
 
-    let args: Vec<String>  = env::args().collect();
+    let args: Vec<String> = env::args().collect();
 
+    let config = Config::build(&args).unwrap_or_else(|err| {
+        println!("Problem parsing arguments: {err}");
+        process::exit(1);
+    });
 
-    let query = &args[1];
-    let file_path = &args[2];
+    println!("Searching for {}", config.query);
+    println!("In file {}", config.file_path);
 
-
-    println!("Searching for {}", query);
-    println!("In file {}", file_path);
-
-
-    let contents = fs::read_to_string(file_path)
-        .expect("Should have been able to read the file");
-
-    println!("With text:\n{contents}");
-
-
-
+    if let Err(e) = run(config) {
+        println!("Application error: {e}");
+        process::exit(1);
+    }
 }
